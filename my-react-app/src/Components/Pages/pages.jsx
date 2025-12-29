0a5bfa7c51c7a20ebaec08a269bc09e041c2d1e2
Pawani-Nethmini <pawaninethmini969@gmail.com>
2025-12-29 11:12:35 +0530

    create some pages

---
+++ my-react-app/src/Components/Pages/pages.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from "../common/header/Header"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+const Pages = () => {
+  return (
+    <>
+      <Router>
+        <Header/>
+        <Switch>
+          {/*<Route path='/about'>
+            <About/>
+          </Route>*/}
+        </Switch>
+      </Router>
+    </>
+  )
+}
+
+export default Pages
