86437b2f706c0a75c822c09ba09c633583908903
Pawani-Nethmini <pawaninethmini969@gmail.com>
2025-12-29 20:02:05 +0530

    blog, back, heading and data

--- my-react-app/src/Components/blog/Blog.jsx
+++ my-react-app/src/Components/blog/Blog.jsx
@@ -4,7 +4,7 @@ import RecentCard from "../home/recent/RecentCard"
 import "../home/recent/recent.css"
 import img from "../images/about.jpg"
 
-const Blog = () {
+const Blog = () => {
     return (
         <>
             <section className="blog-out mb">
